89785235baf5fe74f018610d50ad1e28990963ec
FUJI0130 <sfujita19850130@gmail.com>
2023-08-01 14:29:09 +0900

    UMLの修正

--- src/core/infra/controllers/controller.go
+++ src/core/infra/controllers/controller.go
@@ -4,7 +4,4 @@ import "github.com/gin-gonic/gin"
 
 func InitControllers(r *gin.Engine) {
 	InitHealthController(r)
-	// Here you can add other controllers initializers
-	// InitUserController(r)
-	// InitSomeOtherController(r)
 }
