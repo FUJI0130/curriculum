929f27d000f224bbb7fcab037b502a0d3742ab89
FUJI0130 <sfujita19850130@gmail.com>
2023-10-18 15:45:00 +0900

    一旦保存

--- src/core/infra/middleware/Transaction_handling_test.go
+++ src/core/infra/middleware/Transaction_handling_test.go
@@ -3,6 +3,7 @@ package middleware
 import (
 	"context"
 	"fmt"
+	"os"
 	"testing"
 	"time"
 
@@ -11,6 +12,7 @@ import (
 	"github.com/FUJI0130/curriculum/src/core/domain/userdm"
 	"github.com/FUJI0130/curriculum/src/core/infra/rdbimpl"
 	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/jmoiron/sqlx"
 	//... [snip]
 )
@@ -42,6 +44,7 @@ func initializeDB(db *sqlx.DB) error {
 }
 
 func TestTransactionHandling(t *testing.T) {
+	selectedTestCase := os.Getenv("SELECTED_TEST_CASE")
 	// DBの接続情報を設定ファイルから取得
 	db, err := sqlx.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=True&loc=Local",
 		config.Env.TestDbUser,
@@ -63,7 +66,7 @@ func TestTransactionHandling(t *testing.T) {
 		{
 			name: "正常なリクエスト",
 			input: &userapp.CreateUserRequest{
-				Name:     "TestNameUser2",
+				Name:     "TestNameUser",
 				Email:    "validEmail@gmail.com",
 				Password: "password12345",
 				Skills: []userapp.SkillRequest{
@@ -94,10 +97,145 @@ func TestTransactionHandling(t *testing.T) {
 			},
 			wantError: false,
 		},
-		// 追加のテストケース...
+		{
+			name: "無効なEメールアドレス",
+			input: &userapp.CreateUserRequest{
+				Name:     "InvalidEmailUser",
+				Email:    "invalidEmail",
+				Password: "password12345",
+				Skills: []userapp.SkillRequest{
+					{
+						TagName:    "RDBMS",
+						Evaluation: 2,
+						Years:      26,
+					},
+					{
+						TagName:    "AWS",
+						Evaluation: 4,
+						Years:      26,
+					},
+					{
+						TagName:    "docker",
+						Evaluation: 5,
+						Years:      26,
+					},
+				},
+				Profile: "Software Developer",
+				Careers: []userapp.CareersRequest{
+					{
+						Detail: "Worked at XYZ Corp",
+						AdFrom: time.Date(2020, 1, 1, 0, 0, 0, 0, time.UTC),
+						AdTo:   time.Date(2023, 12, 31, 0, 0, 0, 0, time.UTC),
+					},
+				},
+			},
+			wantError: false,
+		},
+		{
+			name: "既に存在するEメールアドレス",
+			input: &userapp.CreateUserRequest{
+				Name:     "ExistingEmailUser",
+				Email:    "validEmail@gmail.com", // 既存のメールアドレスと同じ
+				Password: "password12345",
+				Skills: []userapp.SkillRequest{
+					{
+						TagName:    "RDBMS",
+						Evaluation: 2,
+						Years:      26,
+					},
+					{
+						TagName:    "AWS",
+						Evaluation: 4,
+						Years:      26,
+					},
+					{
+						TagName:    "docker",
+						Evaluation: 5,
+						Years:      26,
+					},
+				},
+				Profile: "Software Developer",
+				Careers: []userapp.CareersRequest{
+					{
+						Detail: "Worked at XYZ Corp",
+						AdFrom: time.Date(2020, 1, 1, 0, 0, 0, 0, time.UTC),
+						AdTo:   time.Date(2023, 12, 31, 0, 0, 0, 0, time.UTC),
+					},
+				},
+			},
+			wantError: false,
+		},
+		{
+			name: "パスワードが短すぎる",
+			input: &userapp.CreateUserRequest{
+				Name:     "ShortPasswordUser",
+				Email:    "shortPasswordEmail@gmail.com",
+				Password: "pass",
+				Skills: []userapp.SkillRequest{
+					{
+						TagName:    "RDBMS",
+						Evaluation: 2,
+						Years:      26,
+					},
+					{
+						TagName:    "AWS",
+						Evaluation: 4,
+						Years:      26,
+					},
+					{
+						TagName:    "docker",
+						Evaluation: 5,
+						Years:      26,
+					},
+				},
+				Profile: "Software Developer",
+				Careers: []userapp.CareersRequest{
+					{
+						Detail: "Worked at XYZ Corp",
+						AdFrom: time.Date(2020, 1, 1, 0, 0, 0, 0, time.UTC),
+						AdTo:   time.Date(2023, 12, 31, 0, 0, 0, 0, time.UTC),
+					},
+				},
+			},
+			wantError: false,
+		},
+		{
+			name: "スキルが重複",
+			input: &userapp.CreateUserRequest{
+				Name:     "DuplicatedSkillUser",
+				Email:    "duplicatedSkill@gmail.com",
+				Password: "password12345",
+				Skills: []userapp.SkillRequest{
+					{
+						TagName:    "RDBMS",
+						Evaluation: 2,
+						Years:      26,
+					},
+					{
+						TagName:    "RDBMS", // 同じスキル名
+						Evaluation: 4,
+						Years:      26,
+					},
+				},
+				Profile: "Software Developer",
+				Careers: []userapp.CareersRequest{
+					{
+						Detail: "Worked at XYZ Corp",
+						AdFrom: time.Date(2020, 1, 1, 0, 0, 0, 0, time.UTC),
+						AdTo:   time.Date(2023, 12, 31, 0, 0, 0, 0, time.UTC),
+					},
+				},
+			},
+			wantError: false,
+		},
 	}
 
 	for _, tt := range tests {
+
+		if selectedTestCase != "" && tt.name != selectedTestCase { // 環境変数が設定されていて、名前が一致しない場合はスキップ
+			continue
+		}
+
 		t.Run(tt.name, func(t *testing.T) {
 			if err := initializeDB(db); err != nil {
 				t.Fatalf("Failed to initialize the database: %v", err)
@@ -133,12 +271,6 @@ func TestTransactionHandling(t *testing.T) {
 				t.Errorf("Error = %v, wantError %v", err, tt.wantError)
 			}
 
-			// 必要に応じて、DBからデータを取得して、期待する結果と比較する検証を行う
-
-			// テスト終了後のデータベースクリーンアップ
-			// if err := initializeDB(db); err != nil {
-			// 	t.Fatalf("Failed to cleanup the database: %v", err)
-			// }
 		})
 	}
 }
